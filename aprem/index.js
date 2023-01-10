f98a1448dc9bbfec1413f531cd402783cc40497d
Max-Baka <maximelanglois@ip-172-31-24-140.eu-west-3.compute.internal>
2023-01-10 14:26:02 +0000

    circle

---
+++ aprem/index.js
@@ -0,0 +1,82 @@
+function exercice6()
+{
+
+}
+
+function exercice5()
+{
+
+}
+
+function exercice4()
+{
+
+}
+
+function exercice3()
+{
+
+}
+
+function exercice2()
+{
+let circle = {
+    color: "red",
+    radius:100,         // L'objet du DOM Canvas
+    x:220,
+    y:140,
+};
+
+
+ let canvasDom = document.querySelector('#ex2');  // Le context utilisé avec Canvas qui donne accès aux librairies de manipulation 2D
+
+let ctx = canvasDom.getContext('2d');   // On dit au contexte que la couleur de remplissage est gris
+
+ctx.fillStyle = '#DDDDDD';  // On rempli le Canvas de gris en fond 
+
+ctx.fillRect(0, 0, canvasDom.width, canvasDom.height);  // On dit au contexte que la couleur de remplissage est rouge 
+
+ctx.fillStyle = circle.color;   // on commence le tracé 
+
+ctx.beginPath();    // on trace un arc fermé (un cercle) 
+
+ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);     // ctx.arc(x, y, rayon, angleDépart, angleFin) attention angle en radians
+
+
+/*ctx.fill(); // on dessine sur le canvas en remplissant le tracé */
+
+
+ctx.stroke();   // On aurait pu dessiner sur le Canvas seulement le contour ! 
+}
+
+function exercice1()
+{
+let  canvasDom  =  document.querySelector('#ex1');
+let  ctx  =  canvasDom.getContext('2d');
+ctx.fillStyle  =  "#FF0000";  // On trace le contour (stroke) d'un carré
+ctx.fillRect(150, 150, 150, 150);   // ctx.strokeRect(x, y, largeur, hauteur);
+}
+
+function initCanvasSize()
+{
+    // setting the size of the canvas
+    let canvasList = document.querySelectorAll("canvas");
+
+    for(canvas of canvasList)
+    {
+        let section = canvas.parentNode;
+
+        canvas.width = section.clientWidth;
+        canvas.height = section.clientHeight;
+    }
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+   initCanvasSize();
+   exercice1();
+   exercice2();
+   exercice3();
+   exercice4();
+   exercice5();
+   exercice6();
+});
